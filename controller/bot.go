49738c5899ff55350a465ef7f2629f30aa8db8fe
MUSTAFA-A-KHAN <mustafakhan62608@gmail.com>
2024-06-12 01:33:35 +0000

    feat: work separately for each chat

--- controller/bot.go
+++ controller/bot.go
@@ -11,13 +11,16 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
+// ChatState holds the state for a specific chat
+type ChatState struct {
+	sync.RWMutex
+	Word string
+	User string
+}
+
 var (
-	explainingWord = struct {
-		sync.RWMutex
-		word   string
-		user   string
-		chatID int64
-	}{}
+	chatStates = make(map[int64]*ChatState)
+	stateMutex = &sync.RWMutex{}
 )
 
 // StartBot initializes and starts the bot
@@ -40,87 +43,102 @@ func StartBot(token string) error {
 
 	for update := range updates {
 		if update.Message != nil {
-			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-
-			switch update.Message.Command() {
-			case "start":
-				view.SendMessage(bot, update.Message.Chat.ID, "Welcome! Use /word to start a game.")
-			case "word":
-				word, err := model.GetRandomWord()
-				if err != nil {
-					view.SendMessage(bot, update.Message.Chat.ID, "Failed to fetch a word.")
-					continue
-				}
-
-				buttons := []tgbotapi.InlineKeyboardButton{
-					tgbotapi.NewInlineKeyboardButtonData("🗣️ Explain", "explain"),
-				}
-
-				explainingWord.Lock()
-				explainingWord.word = word
-				explainingWord.user = ""
-				explainingWord.chatID = update.Message.Chat.ID
-				explainingWord.Unlock()
-
-				view.SendMessageWithButtons(bot, update.Message.Chat.ID, fmt.Sprintf("The word is ready! Click 'Explain' to explain the word."), buttons)
-
-			default:
-				// Handle guesses here
-				explainingWord.RLock()
-				word := explainingWord.word
-				user := explainingWord.user
-				explainingWord.RUnlock()
-
-				if user != "" && service.NormalizeAndCompare(update.Message.Text, word) {
-					view.SendMessage(bot, update.Message.Chat.ID, fmt.Sprintf("Congratulations! %s guessed the word correctly. /word", update.Message.From.UserName))
-					explainingWord.Lock()
-					explainingWord.word = ""
-					explainingWord.user = ""
-					explainingWord.chatID = 0
-					explainingWord.Unlock()
-				} else if user != "" {
-					view.SendMessage(bot, update.Message.Chat.ID, "That's not correct. Try again!")
-				}
-			}
-
-		}
-
-		if update.CallbackQuery != nil {
-			callback := update.CallbackQuery
-			switch callback.Data {
-			case "explain":
-				explainingWord.Lock()
-				if explainingWord.user != callback.From.UserName && explainingWord.user != "" {
-					bot.AnswerCallbackQuery(tgbotapi.NewCallbackWithAlert(callback.ID, fmt.Sprintf("%s is already explaining the word. %s", explainingWord.user, callback.From.UserName)))
-					// view.SendMessage(bot, callback.Message.Chat.ID, fmt.Sprintf("%s is already explaining the word.", explainingWord.user))
-					explainingWord.Unlock()
-					continue
-				}
-				explainingWord.user = callback.From.UserName
-				explainingWord.Unlock()
-				bot.AnswerCallbackQuery(tgbotapi.NewCallbackWithAlert(callback.ID, explainingWord.word)) /**uncomment if you need it for multiusers */
-				view.SendMessage(bot, callback.Message.Chat.ID, fmt.Sprintf("%s is explaining the word:", callback.From.UserName))
-			default:
-				explainingWord.RLock()
-				word := explainingWord.word
-				//user := explainingWord.user
-				explainingWord.RUnlock()
-				fmt.Println("%s ==%s ", update.Message.Text, word)
-				// if strings.EqualFold(update.Message.Text, word) {
-				if service.NormalizeAndCompare(update.Message.Text, word) {
-					view.SendMessage(bot, callback.Message.Chat.ID, fmt.Sprintf("Congratulations! %s guessed the word correctly.", update.Message.From.UserName))
-					explainingWord.Lock()
-					explainingWord.word = ""
-					explainingWord.user = ""
-					explainingWord.chatID = 0
-					explainingWord.Unlock()
-				} else {
-					view.SendMessage(bot, callback.Message.Chat.ID, "That's not correct. Try again!")
-				}
-			}
-			bot.AnswerCallbackQuery(tgbotapi.NewCallback(callback.ID, ""))
+			handleMessage(bot, update.Message)
+		} else if update.CallbackQuery != nil {
+			handleCallbackQuery(bot, update.CallbackQuery)
 		}
 	}
 
 	return nil
+}
+
+func handleMessage(bot *tgbotapi.BotAPI, message *tgbotapi.Message) {
+	chatID := message.Chat.ID
+
+	stateMutex.Lock()
+	if _, exists := chatStates[chatID]; !exists {
+		chatStates[chatID] = &ChatState{}
+	}
+	chatState := chatStates[chatID]
+	stateMutex.Unlock()
+
+	log.Printf("[%s] %s", message.From.UserName, message.Text)
+
+	switch message.Command() {
+	case "start":
+		view.SendMessage(bot, message.Chat.ID, "Welcome! Use /word to start a game.")
+	case "word":
+		word, err := model.GetRandomWord()
+		if err != nil {
+			view.SendMessage(bot, message.Chat.ID, "Failed to fetch a word.")
+			return
+		}
+
+		buttons := []tgbotapi.InlineKeyboardButton{
+			tgbotapi.NewInlineKeyboardButtonData("🗣️ Explain", "explain"),
+		}
+
+		chatState.Lock()
+		chatState.Word = word
+		chatState.User = ""
+		chatState.Unlock()
+
+		view.SendMessageWithButtons(bot, message.Chat.ID, fmt.Sprintf("The word is ready! Click 'Explain' to explain the word."), buttons)
+
+	default:
+		chatState.RLock()
+		word := chatState.Word
+		user := chatState.User
+		chatState.RUnlock()
+
+		if user != "" && service.NormalizeAndCompare(message.Text, word) {
+			view.SendMessage(bot, message.Chat.ID, fmt.Sprintf("Congratulations! %s guessed the word correctly.\n /word", message.From.UserName))
+			chatState.Lock()
+			chatState.Word = ""
+			chatState.User = ""
+			chatState.Unlock()
+		} else if user != "" {
+			view.SendMessage(bot, message.Chat.ID, "That's not correct. Try again!")
+		}
+	}
+}
+
+func handleCallbackQuery(bot *tgbotapi.BotAPI, callback *tgbotapi.CallbackQuery) {
+	chatID := callback.Message.Chat.ID
+
+	stateMutex.Lock()
+	if _, exists := chatStates[chatID]; !exists {
+		chatStates[chatID] = &ChatState{}
+	}
+	chatState := chatStates[chatID]
+	stateMutex.Unlock()
+
+	switch callback.Data {
+	case "explain":
+		chatState.Lock()
+		if chatState.User != callback.From.UserName && chatState.User != "" {
+			bot.AnswerCallbackQuery(tgbotapi.NewCallbackWithAlert(callback.ID, fmt.Sprintf("%s is already explaining the word. %s", chatState.User, callback.From.UserName)))
+			chatState.Unlock()
+			return
+		}
+		chatState.User = callback.From.UserName
+		chatState.Unlock()
+		bot.AnswerCallbackQuery(tgbotapi.NewCallbackWithAlert(callback.ID, chatState.Word))
+		view.SendMessage(bot, callback.Message.Chat.ID, fmt.Sprintf("%s is explaining the word:", callback.From.UserName))
+	default:
+		chatState.RLock()
+		word := chatState.Word
+		chatState.RUnlock()
+		fmt.Printf("%s == %s ", callback.Message.Text, word)
+		if service.NormalizeAndCompare(callback.Message.Text, word) {
+			view.SendMessage(bot, callback.Message.Chat.ID, fmt.Sprintf("Congratulations! %s guessed the word correctly.", callback.From.UserName))
+			chatState.Lock()
+			chatState.Word = ""
+			chatState.User = ""
+			chatState.Unlock()
+		} else {
+			view.SendMessage(bot, callback.Message.Chat.ID, "That's not correct. Try again!")
+		}
+	}
+	bot.AnswerCallbackQuery(tgbotapi.NewCallback(callback.ID, ""))
 }
