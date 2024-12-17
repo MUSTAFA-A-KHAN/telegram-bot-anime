2e3b801c16c1f57b2b12e743564825161085db07
MUSTAFA-A-KHAN <70553059+MUSTAFA-A-KHAN@users.noreply.github.com>
2024-12-17 18:33:52 +0530

    Revert "Bot respond with a sticker at the start."

--- controller/bot.go
+++ controller/bot.go
@@ -109,7 +109,6 @@ func handleMessage(bot *tgbotapi.BotAPI, message *tgbotapi.Message) {
 		chatState.Word = word
 		chatState.User = ""
 		chatState.Unlock()
-		view.SendSticker(bot, chatID, "CAACAgUAAxkBAAEwCnNnYW-OkgV7Odt9osVwoBSzLC6vsAACMhMAAj45CFdCstMoIYiPfjYE")
 
 		// Send a message with the word and the explain button.
 		view.SendMessageWithButtons(bot, message.Chat.ID, fmt.Sprintf("The word is ready! Click 'Explain' to explain the word."), buttons)
