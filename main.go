2a143bba136ca02c296674d5247cfd3956026bbb
MUSTAFA-A-KHAN-2 <mkhan62609@gmail.com>
2024-12-24 10:50:50 +0000

    feat: add category bot in go routine

--- main.go
+++ main.go
@@ -2,14 +2,58 @@ package main
 
 import (
 	"log"
+	"sync"
 
 	"github.com/MUSTAFA-A-KHAN/telegram-bot-anime/controller"
+	categorybot "github.com/MUSTAFA-A-KHAN/telegram-bot-anime/controller/categoryBot"
 )
 
 func main() {
-	botToken := "6898558980:AAFIMHCvgfDugzk8NSxVPoZ-TKMNyahVPOI"
+	charades := "6898558980:AAFswe1zihkO2xAnKRQCAoLzt0gLW1uMJ88"
+	categorycharades := "7563609270:AAFG12c-eWTKt8L2v_IN8pPF-HsmDDZGVgo"
+	// WaitGroup to wait for all goroutines to finish
+	var wg sync.WaitGroup
+
+	// Add two tasks to the WaitGroup
+	wg.Add(2)
+
+	// Start both bots in separate goroutines
+	go runWordBot(charades, &wg)
+	go runCategoryBot(categorycharades, &wg)
+
+	// Wait for both goroutines to complete
+	wg.Wait()
+
+	log.Println("All bots stopped running.")
+}
+
+// Function to start a bot with error handling
+func runWordBot(botToken string, wg *sync.WaitGroup) {
+	defer wg.Done() // Decrement the wait group counter when this goroutine completes
 	err := controller.StartBot(botToken)
 	if err != nil {
-		log.Panic("error strting bot: ", err)
+		log.Printf("Error starting bot with token %s: %v\n", botToken, err)
 	}
+
+}
+
+// // Function to start a bot with error handling
+// func runAnimeBot(botToken string, wg *sync.WaitGroup) {
+// 	defer wg.Done() // Decrement the wait group counter when this goroutine completes
+// 	word, err := model.GetRandomCar()
+// 	if err != nil {
+// 		log.Printf("Error starting bot with token %s: %v\n", botToken, err)
+// 	}
+// 	fmt.Println("word", word)
+
+// }
+
+// Function to start a bot with error handling
+func runCategoryBot(botToken string, wg *sync.WaitGroup) {
+	defer wg.Done() // Decrement the wait group counter when this goroutine completes
+	err := categorybot.StartBot(botToken)
+	if err != nil {
+		log.Printf("Error starting bot with token %s: %v\n", botToken, err)
+	}
+
 }
