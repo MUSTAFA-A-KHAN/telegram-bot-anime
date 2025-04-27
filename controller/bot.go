b674796dc8b489d8465b057f368369e534247bce
MUSTAFA-A-KHAN <70553059+MUSTAFA-A-KHAN@users.noreply.github.com>
2025-04-27 18:38:24 +0000

    refactor: another way

--- controller/bot.go
+++ controller/bot.go
@@ -100,7 +100,7 @@ func handleMessage(bot *tgbotapi.BotAPI, message *tgbotapi.Message) {
 		}
 	case "start":
 		// Send a welcome message with instructions to start the game.
-		variable := os.Getenv("TEST")
+		variable := os.Getenv("SHARED_TEST")
 		fmt.Print("Test:" + os.Getenv("TEST"))
 		view.SendMessage(bot, message.Chat.ID, "Welcome! Use /word to start a game.test-"+variable)
 	case "stats":
