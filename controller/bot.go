55d5006f810a856f668a94f1a0dada1f8c856a0b
MUSTAFA-A-KHAN-2 <mkhan62609@gmail.com>
2025-05-20 06:38:06 +0000

    refactor: fix port

--- controller/bot.go
+++ controller/bot.go
@@ -574,5 +574,5 @@ func startHTTPServer() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Bot is running!")
 	})
-	log.Fatal(http.ListenAndServe(":8081", nil))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
